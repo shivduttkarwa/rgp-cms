06180d867dd4ff1de10c66d2dce3fb90dd8ee42a
Shivdutt Karwa <shivduttkarwa@gmail.com>
2026-04-08 16:08:05 +0530

    fix

--- backend/listings/wagtail_hooks.py
+++ backend/listings/wagtail_hooks.py
@@ -8,6 +8,7 @@ class ListingViewSet(SnippetViewSet):
     menu_label = "Listings"
     menu_icon = "home"
     menu_order = 200
+    add_to_admin_menu = True
     list_display = ["title", "category", "location", "price", "is_new", "has_detail_page"]
     list_filter = ["category", "is_new", "featured"]
     search_fields = ["title", "location"]
